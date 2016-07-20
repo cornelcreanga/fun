779e21d6893e15006dc20bf24947fca838aa284a
cornel creanga <cornelcreanga@yahoo.com>
2016-07-20 10:40:35 +0300

    update dependencies add a new puzzle

---
+++ src/main/test/com/ccreanga/various/strings/MaximumNumberTest.java
@@ -0,0 +1,39 @@
+package com.ccreanga.various.strings;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaximumNumberTest {
+    @Test
+    public void computeMaximumNumber() throws Exception {
+        String[] s = new String[]{"32","324","321"};
+        Assert.assertEquals("32432321",MaximumNumber.computeMaximumNumber(s));
+
+
+        s = new String[]{"32","324","321","0","5"};
+        Assert.assertEquals("5324323210",MaximumNumber.computeMaximumNumber(s));
+
+        boolean caught = false;
+        try{
+            s = new String[]{"32","a"};
+            MaximumNumber.computeMaximumNumber(s);
+        }catch (RuntimeException e){
+            caught = true;
+        }
+        Assert.assertTrue(caught);
+
+        caught = false;
+        try{
+            s = new String[]{"32","-1"};
+            MaximumNumber.computeMaximumNumber(s);
+        }catch (RuntimeException e){
+            caught = true;
+        }
+        Assert.assertTrue(caught);
+
+    }
+
+
+}
