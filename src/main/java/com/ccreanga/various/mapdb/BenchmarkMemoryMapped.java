c6a50aa3ac7dacf98ab4e2a57335f7946061b1f7
cornelcreanga <cornelcreanga@yahoo.com>
2019-03-19 22:10:19 +0200

    add linux data

--- src/main/java/com/ccreanga/various/mapdb/BenchmarkMemoryMapped.java
+++ src/main/java/com/ccreanga/various/mapdb/BenchmarkMemoryMapped.java
@@ -41,22 +41,22 @@ public class BenchmarkMemoryMapped {
         }
     }
 
-    @Benchmark
-    @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    @Fork(value = 1)
-    @Warmup(iterations = 2)
-    @Measurement(iterations = 5)
+//    @Benchmark
+//    @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+//    @Fork(value = 1)
+//    @Warmup(iterations = 2)
+//    @Measurement(iterations = 5)
     public static void randomAccessMemory(){
         for (int i = 0; i < ITERATIONS; i++) {
             jdkMap.get("item"+(long)(Math.random()*MAP_SIZE));
         }
     }
 
-    @Benchmark
-    @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
-    @Fork(value = 1)
-    @Warmup(iterations = 2)
-    @Measurement(iterations = 5)
+//    @Benchmark
+//    @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+//    @Fork(value = 1)
+//    @Warmup(iterations = 2)
+//    @Measurement(iterations = 5)
     public static void randomAccessMemoryMapped(){
         DB db = DBMaker
                 .fileDB("/tmp/file.db")
@@ -71,29 +71,35 @@ public class BenchmarkMemoryMapped {
         db.close();
     }
 
-//    @Benchmark
-//    @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
-//    @Fork(value = 1)
-//    @Warmup(iterations = 2)
-//    @Measurement(iterations = 5)
-//    public static void randomAccessNonMemoryMapped(){
-//        DB db = DBMaker
-//                .fileDB("/tmp/file.db")
-//                .make();
-//        mapDbNonMemoryMapped = db
-//                .hashMap("map", Serializer.STRING, Serializer.LONG)
-//                .createOrOpen();
-//        for (int i = 0; i < ITERATIONS; i++) {
-//            mapDbNonMemoryMapped.get("item"+(long)(Math.random()*MAP_SIZE));
-//        }
-//        db.close();
-//    }
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime) @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @Fork(value = 1)
+    @Warmup(iterations = 2)
+    @Measurement(iterations = 5)
+    public static void randomAccessNonMemoryMapped(){
+        DB db = DBMaker
+                .fileDB("/tmp/file.db")
+                .make();
+        mapDbNonMemoryMapped = db
+                .hashMap("map", Serializer.STRING, Serializer.LONG)
+                .createOrOpen();
+        for (int i = 0; i < ITERATIONS; i++) {
+            mapDbNonMemoryMapped.get("item"+(long)(Math.random()*MAP_SIZE));
+        }
+        db.close();
+    }
 
     /**
-     * osx
+     * osx/job
      Benchmark                                       Mode  Cnt     Score    Error  Units
      BenchmarkMemoryMapped.randomAccessMemory        avgt    5    69.033 ±  5.212  ms/op
      BenchmarkMemoryMapped.randomAccessMemoryMapped  avgt    5  1800.507 ± 30.767  ms/op
+
+     linux/home
+     Benchmark                                       Mode  Cnt     Score    Error  Units
+     BenchmarkMemoryMapped.randomAccessMemory        avgt    5    60.496 ±  0.439  ms/op
+     BenchmarkMemoryMapped.randomAccessMemoryMapped  avgt    5  1257.855 ± 32.354  ms/op
+     BenchmarkMemoryMapped.randomAccessNonMemoryMapped  avgt    5  30739.443 ± 408.829  ms/op
      */
 
     public static void main(String[] args) throws RunnerException {
