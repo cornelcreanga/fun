0d8b015a7021dbbb879aac44d3b9d8583ad73873
cornelcreanga <cornelcreanga@yahoo.com>
2019-05-03 18:41:40 +0300

    fix triessize benchmark

--- src/main/java/com/ccreanga/various/tries/TriesSizeBenchmark.java
+++ src/main/java/com/ccreanga/various/tries/TriesSizeBenchmark.java
@@ -3,22 +3,46 @@ package com.ccreanga.various.tries;
 import static com.ccreanga.various.util.FormatUtil.readableSize;
 
 import it.unimi.dsi.bits.TransformationStrategies;
+import it.unimi.dsi.bits.TransformationStrategy;
+import it.unimi.dsi.fastutil.io.BinIO;
+import it.unimi.dsi.io.FastBufferedReader;
+import it.unimi.dsi.io.LineIterator;
+import it.unimi.dsi.lang.MutableString;
+import it.unimi.dsi.logging.ProgressLogger;
 import it.unimi.dsi.sux4j.util.ZFastTrie;
-import java.nio.charset.Charset;
-import java.nio.file.Paths;
-import java.util.List;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
 import org.ehcache.sizeof.SizeOf;
 
 public class TriesSizeBenchmark {
 
     public static void main(String[] args) throws Exception {
         SizeOf sizeOf = SizeOf.newInstance();
-        List<String> lines = java.nio.file.Files.readAllLines(Paths.get(ClassLoader.getSystemResource("words.txt").toURI()), Charset.forName("utf-8"));
-        ZFastTrie<String> trie = new ZFastTrie<>(TransformationStrategies.utf16());
-        trie.addAll(lines);
-        long compressedSize = sizeOf.deepSizeOf(trie);
+
+        final InputStream inputStream = TriesSizeBenchmark.class.getClassLoader().getResourceAsStream("words.txt");
+        Iterator<MutableString> lineIterator = new LineIterator(new FastBufferedReader(new InputStreamReader(inputStream, "UTF-8")));
+        //if (jsapResult.userSpecified("loadAll"))
+        lineIterator = ((LineIterator) lineIterator).allLines().iterator();
+
+        final TransformationStrategy<CharSequence> transformationStrategy = TransformationStrategies.prefixFreeIso();
+
+        final ProgressLogger pl = new ProgressLogger();
+        pl.displayLocalSpeed = true;
+        pl.displayFreeMemory = true;
+        pl.itemsName = "keys";
+        pl.start("Adding keys...");
+
+        final ZFastTrie<CharSequence> zFastTrie = new ZFastTrie<>(transformationStrategy);
+        while (lineIterator.hasNext()) {
+            zFastTrie.add(lineIterator.next().copy());
+            pl.lightUpdate();
+        }
+        pl.done();
+        long compressedSize = sizeOf.deepSizeOf(zFastTrie);
         System.out.println(readableSize(compressedSize));
 
+        BinIO.storeObject(zFastTrie, "/home/cornel/tree.bin");
 
     }
 
